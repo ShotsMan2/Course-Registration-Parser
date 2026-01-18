63ab63057d163b1b6ee044632c829c34680020a5
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 22:57:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,51 +4,58 @@
 int main()
 {
     char kayit[99];
-    int i=0,len=0,sum=0,currentlen=0,maxlen=0,maxstartindex=0,currentstartindex=0;
+    int i = 0, len = 0, sum = 0, currentlen = 0, maxlen = 0, maxstartindex = 0, currentstartindex = 0;
 
     printf("kayit giriniz ");
-    scanf("%s",kayit);
+    scanf("%s", kayit);
 
-    while(kayit[i]!='\0'){
+    while (kayit[i] != '\0')
+    {
         i++;
         len++;
     }
 
-    for(i=0;i<=len;i++){
-        if(kayit[i]=='\0' || kayit[i]=='#'){
+    for (i = 0; i <= len; i++)
+    {
+        if (kayit[i] == '\0' || kayit[i] == '#')
+        {
             sum++;
 
-            if(currentlen>maxlen){
-                maxlen=currentlen;
-                maxstartindex=currentstartindex;
+            if (currentlen > maxlen)
+            {
+                maxlen = currentlen;
+                maxstartindex = currentstartindex;
             }
 
-            currentlen=0;
-            currentstartindex=i+1;
+            currentlen = 0;
+            currentstartindex = i + 1;
         }
-        else{
+        else
+        {
             currentlen++;
         }
     }
 
-    printf("\n Total course count: %d\n",sum);
+    printf("\n Total course count: %d\n", sum);
 
-    for(i=0;i<maxlen;i++){
-        printf("%c",kayit[i+maxstartindex]);
+    for (i = 0; i < maxlen; i++)
+    {
+        printf("%c", kayit[i + maxstartindex]);
     }
 
-    printf("\n maxlen: %d\n",maxlen);
+    printf("\n maxlen: %d\n", maxlen);
 
-    for(i=0;i<len;i++){
-        if(kayit[i]=='#'){
+    for (i = 0; i < len; i++)
+    {
+        if (kayit[i] == '#')
+        {
             printf("\n");
         }
-        else{
-            printf("%c",kayit[i]);
+        else
+        {
+            printf("%c", kayit[i]);
         }
     }
-
-
 
     return 0;
 }
